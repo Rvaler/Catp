cce86def71a54e2118becb9cf7bc14018592dae2
Rafael Valer <rvaler93@gmail.com>
2014-10-09 21:12:53 -0300

    Mensagem

--- catp10.rb
+++ catp10.rb
@@ -5,16 +5,24 @@ require 'date'
 
 class Pessoa
 	
+	attr_accessor :nome, :data, :sexo, :local, :idade
 
 	#constructor (nome, data_nascimento, sexo, localizacao)
 	def initialize(nome = "Indefinido", data = nil, sexo = "Indefinido", local = "0.0")
 
 		#validations
-
+		today = Date.today
+		
 		if(data!=nil)
+
 			born = Date.parse data
-			@idade = age(born)
-			@data = data.to_s
+			if(born <= today)
+				@idade = age(born)
+				@data = data.to_s
+			else
+				@data = Date.today.to_s	
+				@idade = "Indefinido"			
+			end
 		else
 			@data = Date.today.to_s	
 			@idade = "Indefinido"
@@ -62,7 +70,7 @@ class Pessoa
 	end
 
 	def getNascimento
-		puts "Data de Nascimento: " + @data
+		puts "Data de Nascimento: " + @data.to_s
 	end
 
 	def getSexo
@@ -79,28 +87,31 @@ class Pessoa
 
 	def printInfo
 		getNome
-		getIdade
-		getLocal
-		getSexo
 		getNascimento
+		getIdade
+		getSexo
+		getLocal
 	end
 
-	attr_accessor :nome, :data, :sexo, :local, :idade
+	
 	
 end
 
 
 
 #p = Pessoa.new("Joao da Silva", "10/10/1993", )
-p = Pessoa.new
-p.nome = "joao"
-puts p.nome
-puts p.sexo
-#p.nome = "joao"
-#p.local = 5.5
-#p.data = '23-10-1993'
-p.printInfo
+pessoa1 = Pessoa.new("Joao Silva", "07/10/2002", "masculino", "7.5")
 
+pessoa2 = Pessoa.new
+
+pessoa2.nome = "Maria Soares"
+pessoa2.data = "25-03-1975"
+pessoa2.sexo = "Feminino"
+pessoa2.idade = 23
+
+pessoa1.printInfo
+puts 
+pessoa2.printInfo
 
 
 
